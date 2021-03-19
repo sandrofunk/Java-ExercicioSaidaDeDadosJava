be6fe115bf536fef87b11df8dec33e6d615feff0
sandrofunk <sagfunk@gmail.com>
2021-03-19 17:52:54 -0300

    Exercicio de Saida de Dados

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String produtoA = "Computador";
+		String produtoB = "Celular";
+		
+		int idade = 30;
+		int codigo = 5000;
+		char genero = 'F';
+		
+		double preco1 = 2100.0;
+		double preco2 = 650.0;
+		double medida = 5.212411;
+		
+		System.out.println("Produtos");
+		System.out.printf("Computador, no valor de R$ %.2f.", preco1);
+		System.out.println();
+		System.out.printf("Celular, no valor de R$ %.2f.", preco2);
+		System.out.println();
+		System.out.printf("O sexo da pessoa é %s ela tem %d anos. %n", genero, idade);		
+		System.out.printf("O preço do pacote A é %.2f, o preço do pacote B é %.2f.%n", preco1, preco2);
+		System.out.printf("O código do produto é %d.%n", codigo);
+		System.out.printf("A medida da caixa é %.4f.", medida);
+		
+		
+	
+		
+		
+		
+
+	}
+
+}
